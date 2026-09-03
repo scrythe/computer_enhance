fbaebdeddb1bd6c9b043bc025140670088d73da2
Magomed <git@magomed.dev>
2026-09-03 15:29:03 +0200

    feat: print to buffer and compare bin with nasm
    attempt to improve 0039 a little and made it so that I print to buffer
    instead of using std.debug.print. Also gives the output_slice to nasm
    and compares its binary output with expected binary output.

--- src/listing_0039_more_movs.zig
+++ src/listing_0039_more_movs.zig
@@ -41,7 +41,31 @@ pub fn main(init: std.process.Init) !void {
     const listing_0039_paths = [_][]const u8{ "computer_enhance", "perfaware", "part1", "listing_0039_more_movs" };
     const listing_0039_path = try std.fs.path.join(arena_alloc, &listing_0039_paths);
     const content_listing_0039: []u8 = try Io.Dir.cwd().readFileAlloc(io, listing_0039_path, arena_alloc, .unlimited);
+    const max_chars_per_instruction = "mov bx, [bx + si + 65535]".len;
+    // could maybe less size since above instruction actually takes 4 bytes but also maybe some combination
+    // could make it buffer overflow?
+    const disassembly_buffer = try arena_alloc.alloc(u8, content_listing_0039.len / 2 * max_chars_per_instruction);
 
+    const disassembly_slice = try disassemble_binary(disassembly_buffer, content_listing_0039);
+    std.debug.print("{s}\n", .{disassembly_slice});
+
+    const listing_0039_disassembly_output_paths = [_][]const u8{ "testing_results", "listing_0039_more_movs_disassembly.asm" };
+    const listing_0039_disassembly_output_path = try std.fs.path.join(arena_alloc, &listing_0039_disassembly_output_paths);
+    try std.Io.Dir.cwd().createDirPath(io, "testing_results");
+    const file_listing_0039_disassembly = try std.Io.Dir.cwd().createFile(io, listing_0039_disassembly_output_path, .{ .truncate = false });
+    try file_listing_0039_disassembly.writeStreamingAll(io, disassembly_slice);
+
+    const nasm_result = try std.process.run(arena_alloc, io, .{
+        .argv = &.{ "nasm", listing_0039_disassembly_output_path, "-o", "/dev/stdout" },
+    });
+
+    try std.testing.expectEqualStrings(content_listing_0039, nasm_result.stdout);
+}
+
+fn disassemble_binary(disassembly_buffer: []u8, content_listing_0039: []u8) ![]u8 {
+    var disassembly_buffer_writer = std.Io.Writer.fixed(disassembly_buffer);
+
+    try disassembly_buffer_writer.print("; listing_0037_single_register_mov disassembly:\nbits 16", .{});
     var i: u32 = 0;
     while (i < content_listing_0039.len) {
         const instruction = content_listing_0039[i] >> 2;
@@ -55,21 +79,22 @@ pub fn main(init: std.process.Init) !void {
             const reg_field = (content_listing_0039[i] >> 3) & 0b00000111;
             const reg_mem_field = content_listing_0039[i] & 0b00000111;
 
-            // const reg_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_field] else reg_bits_name_map[reg_field];
-            // const reg_mem_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_mem_field] else reg_bits_name_map[reg_mem_field];
+            const reg_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_field] else reg_bits_name_map[reg_field];
+            const reg_mem_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_mem_field] else reg_bits_name_map[reg_mem_field];
+            const reg_mem_effective_calc_type = reg_mem_effective_calc_map[reg_mem_field];
 
             var disp: u16 = undefined;
 
             // no displacement (Except when R/M=110, then 16 bit displacement)
             if (mod == 0b000000) {
-                const reg_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_field] else reg_bits_name_map[reg_field];
-                const reg_mem_effective_calc = reg_mem_effective_calc_map[reg_mem_field];
-                // TODO: handle direct address
+                // NOTE: skipping direct addresss, only relevant in 0040
                 std.debug.assert(reg_mem_field != 0b110);
                 if (direction_bit == 1) {
-                    std.debug.print("mov {s}, [{s}]\n", .{ reg_field_name, reg_mem_effective_calc });
+                    // e.g.: mov cx, [bx + si]
+                    try disassembly_buffer_writer.print("\nmov {s}, [{s}]", .{ reg_field_name, reg_mem_effective_calc_type });
                 } else {
-                    std.debug.print("mov [{s}], {s}\n", .{ reg_mem_effective_calc, reg_field_name });
+                    // e.g.: mov [bx + si], cx
+                    try disassembly_buffer_writer.print("\nmov [{s}], {s}", .{ reg_mem_effective_calc_type, reg_field_name });
                 }
             }
             // 8 bit displacement
@@ -77,12 +102,10 @@ pub fn main(init: std.process.Init) !void {
                 i += 1;
                 disp = content_listing_0039[i];
 
-                const reg_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_field] else reg_bits_name_map[reg_field];
-                const reg_mem_effective_calc = reg_mem_effective_calc_map[reg_mem_field];
                 if (direction_bit == 1) {
-                    std.debug.print("mov {s}, [{s} + {d}]\n", .{ reg_field_name, reg_mem_effective_calc, disp });
+                    try disassembly_buffer_writer.print("\nmov {s}, [{s} + {d}]", .{ reg_field_name, reg_mem_effective_calc_type, disp });
                 } else {
-                    std.debug.print("mov [{s} + {d}], {s}\n", .{ reg_mem_effective_calc, disp, reg_field_name });
+                    try disassembly_buffer_writer.print("\nmov [{s} + {d}], {s}", .{ reg_mem_effective_calc_type, disp, reg_field_name });
                 }
             }
             // 16 bit displacement
@@ -93,26 +116,21 @@ pub fn main(init: std.process.Init) !void {
                 const disp_high: u16 = content_listing_0039[i];
                 disp = (disp_high << 8) | disp_lo;
 
-                const reg_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_field] else reg_bits_name_map[reg_field];
-                const reg_mem_effective_calc = reg_mem_effective_calc_map[reg_mem_field];
                 if (direction_bit == 1) {
-                    std.debug.print("mov {s}, [{s} + {d}]\n", .{ reg_field_name, reg_mem_effective_calc, disp });
+                    try disassembly_buffer_writer.print("\nmov {s}, [{s} + {d}]", .{ reg_field_name, reg_mem_effective_calc_type, disp });
                 } else {
-                    std.debug.print("mov [{s} + {d}], {s}\n", .{ reg_mem_effective_calc, disp, reg_field_name });
+                    try disassembly_buffer_writer.print("\nmov [{s} + {d}], {s}", .{ reg_mem_effective_calc_type, disp, reg_field_name });
                 }
             }
             // Register mode (no displacement)
             else {
-                const reg_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_field] else reg_bits_name_map[reg_field];
-                const reg_mem_field_name = if (wide_bit == 1) reg_wide_bits_name_map[reg_mem_field] else reg_bits_name_map[reg_mem_field];
                 if (direction_bit == 1) {
-                    std.debug.print("mov {s}, {s}\n", .{ reg_field_name, reg_mem_field_name });
+                    try disassembly_buffer_writer.print("\nmov {s}, {s}", .{ reg_field_name, reg_mem_field_name });
                 } else {
-                    std.debug.print("mov {s}, {s}\n", .{ reg_mem_field_name, reg_field_name });
+                    try disassembly_buffer_writer.print("\nmov {s}, {s}", .{ reg_mem_field_name, reg_field_name });
                 }
             }
 
-            // std.debug.print("{b:0>8}\n", .{mod});
             i += 1;
         } else if ((instruction >> 2) == IMM_INSTRUCTION) {
             const wide_bit = (content_listing_0039[i] >> 3) & 0b00000001;
@@ -125,13 +143,14 @@ pub fn main(init: std.process.Init) !void {
                 i += 1;
                 const data_high: u16 = content_listing_0039[i];
                 const data = (data_high << 8) | data_low;
-                std.debug.print("mov {s}, {d}\n", .{ reg_field_name, data });
+                try disassembly_buffer_writer.print("\nmov {s}, {d}", .{ reg_field_name, data });
             } else {
                 const reg_field_name = reg_bits_name_map[reg_field];
                 const data = content_listing_0039[i];
-                std.debug.print("mov {s}, {d}\n", .{ reg_field_name, data });
+                try disassembly_buffer_writer.print("\nmov {s}, {d}", .{ reg_field_name, data });
             }
             i += 1;
         }
     }
+    return disassembly_buffer[0..disassembly_buffer_writer.end];
 }
